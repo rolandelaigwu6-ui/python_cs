cbab53b69daf15b48a7f1580c3da45b80f81446e
rolandelaigwu6-ui <rolandelaigwu6@gmail.com>
2026-07-17 12:38:44 +0000

    adding validation that checks if the input the user enter was a valid number

--- scoreboard.py
+++ scoreboard.py
@@ -13,29 +13,52 @@ block of memory that cannot grow or shrink.
 
 The Manual Shift: Every time a new score enters, you must write a loop or index-assignment
 logic that manually shifts elements to the left to clear out index 4 for the incoming score.
+
+
+Step-by-Step Implementation Outline
+
+Break your script down into these logical phases:
+
+Initialization: Create your fixed list of 5 elements and an active loop variable 
+(like while True:) to keep the program running indefinitely until you type "exit".
+
+Input & Validation: Prompt the user to type a score. Ensure the user entered a valid number
+. What happens if they accidentally type a letter? Prevent your program from crashing.
+
+The Shift Logic: Figure out how to update the list indices. Hint: If i goes from 0 to 3
+, how can you pass the value at scores[i+1] down to scores[i]?
+
+Insertion & Processing: Place the new score at the very end of the list. Then, calculate the total sum of 
+the list elements without using the built-in sum() function—write a quick for loop to add them up manually.
+
+Output: Print out both the current state of the array and the calculated rolling sum so you can verify it visually.
 """
 
-scores = [ 0,  0,  0,  0,  0 ]
 
+current_scores_board_list = [0, 0, 0, 0, 0]
 while True:
-    user_input = input("enter a valid score (or type [ EXIT ] to stop the program): ")
-    if user_input.lower() == "exit":
-        print("nice job GoodBye")
-        break
-         
+  user_input = input("Enter A  valid score Ronaldo Scored in the ongoing match \n( or  type [ exit ] if the match has already Ended  ):  ")
+  if user_input.lower() == "exit":
+    print("thanks: GoodBye")
+    break
+  elif not int(user_input):
+    print("oya Bye Bye")
+    break
+  
+  
+  new_score = int(user_input)
+
+  
+
+  for i in range(4):
+    current_scores_board_list[i] = current_scores_board_list[i+1]
+
+    current_scores_board_list[4] = new_score
+    total_score = 0
+    for total_count_ in current_scores_board_list:
+      total_score = total_score+total_count_
+
+    print(f"the current score table {current_scores_board_list} \n total score count in the last five round {total_score}")
 
 
-    new_score = int(user_input)
-    if not int(new_score):
-        print("thanks: oya leave here")
-        break
-
-    for i in range(4):  
-        scores[i] = scores[i+1]
-        total_score = 0
-
-        scores[4] = new_score
-        for scor_sum in scores:
-            total_score += scor_sum
-
-    print(f"updated score, {scores} total score count {total_score}") 
+      
